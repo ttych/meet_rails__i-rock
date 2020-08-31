3491019c07cd6ee31199edfd55a671ac098bc515
Thomas Tych <thomas.tych@gmail.com>
2020-08-31 19:17:34 +0200

    add integration test for achievement controller: create

--- spec/controllers/achievements_controller_spec.rb
+++ spec/controllers/achievements_controller_spec.rb
@@ -28,4 +28,34 @@ describe AchievementsController, type: :controller do
       expect(assigns(:achievement)).to be_a_new(Achievement)
     end
   end
+
+  describe 'POST create' do
+    context 'with valid achievement' do
+      let(:valid_achievement) { FactoryBot.attributes_for(:public_achievement) }
+      it 'redirects to achievements#show' do
+        post :create, params: { achievement: valid_achievement }
+        expect(response).to redirect_to(achievement_path(assigns[:achievement]))
+      end
+
+      it 'creates new achievement in database' do
+        expect do
+          post :create, params: { achievement: valid_achievement }
+        end.to change(Achievement, :count).by(1)
+      end
+    end
+    context 'with invalid achievement' do
+      let(:invalid_achievement) { FactoryBot.attributes_for(:public_achievement, title: '') }
+
+      it 'renders :new template' do
+        post :create, params: { achievement: invalid_achievement }
+        expect(response).to render_template(:new)
+      end
+
+      it "doesn't create new achievement in the database" do
+        expect do
+          post :create, params: { achievement: invalid_achievement }
+        end.to_not change(Achievement, :count)
+      end
+    end
+  end
 end
